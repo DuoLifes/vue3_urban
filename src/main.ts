3c8558d8bee8aae33755b490cc9dc06a919dfc20
张帅 <1564900966@qq.com>
2023-05-31 00:06:51 +0800

    提交

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ import 'element-plus/dist/index.css'
 //svg插件需要配置代码
 import 'virtual:svg-icons-register'
 //引入自定义插件对象:注册整个项目全局组件
-import gloalComponent from '@/components';
+import gloalComponent from '@/components'
 //配置element-plus国际化
-//@ts-ignore
+//@ts-expect-error
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 //暗黑模式需要的样式
 import 'element-plus/theme-chalk/dark/css-vars.css'
@@ -22,18 +22,18 @@ import pinia from './store'
 const app = createApp(App)
 //安装element-plus插件
 app.use(ElementPlus, {
-     locale: zhCn//element-plus国际化配置
+  locale: zhCn, //element-plus国际化配置
 })
 //安装自定义插件
-app.use(gloalComponent);
+app.use(gloalComponent)
 //安装仓库
-app.use(pinia);
+app.use(pinia)
 //注册模板路由
-app.use(router);
+app.use(router)
 //引入路由鉴权文件
-import './permisstion';
+import './permisstion'
 //引入自定义指令文件
-import { isHasButton } from '@/directive/has';
-isHasButton(app);
+import { isHasButton } from '@/directive/has'
+isHasButton(app)
 //将应用挂载到挂载点上
 app.mount('#app')
